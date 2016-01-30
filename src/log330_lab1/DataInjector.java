0c4521331fff5009879410d77ac53363d59c85ab
JS <jeansebastien.ares@gmail.com>
2016-01-30 13:06:16 -0500

    ajout d'une liste y double

--- src/log330_lab1/DataInjector.java
+++ src/log330_lab1/DataInjector.java
@@ -4,23 +4,40 @@ import java.util.ArrayList;
 
 public class DataInjector {
 
-	ArrayList<Integer> listNbre = new ArrayList<Integer>();
+	ArrayList<Integer> listNbrex = new ArrayList<Integer>();
+	ArrayList<Double> listNbrey = new ArrayList<Double>();
 	
-	public ArrayList<Integer> getListNbre(){
-		return listNbre;
-		
+	public ArrayList<Integer> getListNbrex(){
+		return listNbrex;
+	}
+	
+	public ArrayList<Double> getListNbrey(){
+		return listNbrey;
 	}
 	
 	public void setListNbre(){
-		listNbre.add(186);
-		listNbre.add(699);
-		listNbre.add(132);
-		listNbre.add(272);
-		listNbre.add(291);
-		listNbre.add(331);
-		listNbre.add(199);
-		listNbre.add(1890);
-		listNbre.add(788);
-		listNbre.add(1601);
+		listNbrex.add(186);
+		listNbrex.add(699);
+		listNbrex.add(132);
+		listNbrex.add(272);
+		listNbrex.add(291);
+		listNbrex.add(331);
+		listNbrex.add(199);
+		listNbrex.add(1890);
+		listNbrex.add(788);
+		listNbrex.add(1601);
+	}
+	
+	public void setListNbrey(){
+		listNbrey.add(15.0);
+		listNbrey.add(69.9);
+		listNbrey.add(6.5);
+		listNbrey.add(22.4);
+		listNbrey.add(28.4);
+		listNbrey.add(65.9);
+		listNbrey.add(19.4);
+		listNbrey.add(189.7);
+		listNbrey.add(38.8);
+		listNbrey.add(138.2);
 	}
 }
